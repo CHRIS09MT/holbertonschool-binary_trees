6b3ec7a0f0f0d32073608e900a6f80923e36b4a3
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-08-07 13:38:07 -0500

    print a binary tree using in-order traversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -12,11 +12,12 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
-	func(tree->n);
-
 	if (tree->left != NULL)
 		binary_tree_preorder(tree->left, func);
 
+	func(tree->n);
+
 	if (tree->right != NULL)
 		binary_tree_preorder(tree->right, func);
+
 }
